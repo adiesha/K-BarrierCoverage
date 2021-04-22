fe44efb4c87fcd0907955becc9b6b3e7b87468a2
Adiesha <adiesha@gmail.com>
2021-04-21 19:06:09 -0600

    Modified test script

--- testscripts/test.py
+++ testscripts/test.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-import numpy as np
-from scipy.spatial import KDTree
+
+
+def reassign(list):
+  list = [0, 1]
+
+def append(list):
+  list.append(1)
 
 
 def main():
@@ -26,12 +31,24 @@ def main():
         i = 0
         while i < len(path) - 1:
             g2.remove_edge(path[i], path[i + 1])
-            i = i+1
+            i = i + 1
 
     # print(list(nx.node_disjoint_paths(g2, 0, 7)))
     nx.draw_networkx(g2, edge_color='black')
     plt.draw()
     plt.show()
+
+    l1 = list(g2.nodes)
+    print(l1)
+    l1[3] = 5
+    # l1.remove(3)
+    print(l1)
+
+    llist = [0]
+    reassign(llist)
+    print(llist)
+    append(llist)
+    print(llist)
 
 if __name__ == '__main__':
     main()
