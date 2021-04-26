696ca7b415b772fc92397f7a236a792bbcd3426b
kemalturksonmez <kemalturksonmez@gmail.com>
2021-04-26 17:39:42 -0400

    Added coloring

--- main.py
+++ main.py
@@ -2,57 +2,76 @@ from DS import DS
 from generateDisks import generateDisks
 from visualize import visualize
 from residualgraph import ResidualGraph
+import networkx as nx
+def run1():
+    gd = generateDisks()
+    vis = visualize()
+    unitDistance = 2
+    s = 1
+    t = 12
+    belts = (s, t)
+    k = 2
+    # generate optimal k-coverage disk set
+    disks = gd.generateUnitDisks(k, belts, unitDistance)
+    # generate belt rectangles
+    beltRectangles = gd.getBeltRegions(k, belts, unitDistance)
 
-gd = generateDisks()
-vis = visualize()
-unitDistance = 2
-s = 1
-t = 12
-belts = (s, t)
-k = 2
-# generate optimal k-coverage disk set
-disks = gd.generateUnitDisks(k, belts, unitDistance)
+    # visualize circles
+    vis.visualizeCircles(disks, beltRectangles, unitDistance)
 
+    # create residual graph
+    r1 = ResidualGraph(disks, s, t, unitDistance * 2)
+    # starting vertex
+    start = r1.getUindex()
+    # ending vertex
+    end = r1.getVindex()
+    print(r1.getUindexofG, r1.getVindexOfG)
+    print(list(nx.node_disjoint_paths(r1.G, r1.getUindexofG, r1.getVindexOfG)))
+    # visualize G graph
+    vis.visualizeGraph(r1.G, list(nx.node_disjoint_paths(r1.G, r1.getUindexofG, r1.getVindexOfG)), r1.getUindexofG, r1.getVindexOfG)
+
+    # visualize G' graph
+    vis.visualizeGraph(r1.Gp, r1.disjoint_paths, start, end)
+
+    # retrieve residual graph
+    r_Graph = r1.getResidualGraph()
+
+    # visualize residual graph
+    vis.visualizeGraph(r_Graph, [], start, end)
+
+    
+
+run1()
 # generate d random unit disks
 # d = 20
 # disks = gd.generateRandomUnitDisks(d, belts, unitDistance)
 
-# generate belt rectangles
-beltRectangles = gd.getBeltRegions(k, belts, unitDistance)
 
-# visualize circles
-vis.visualizeCircles(disks, beltRectangles, unitDistance)
 
-# create residual graph
-r1 = ResidualGraph(disks, s, t, unitDistance * 2)
 
-# retrieve residual graph
-r_Graph = r1.getResidualGraph()
 
-# visualize residual graph
-# vis.visualizeGraph(r_Graph)
 
 # vis.visualizeGraph(r1.G)
-print(list(r1.G.successors(8)))
+# print(list(r1.G.successors(8)))
 
-ds = DS(r1.G, [r1.getUindexofG(), 9])
-print(ds.IN)
-print(ds.OUT)
+# ds = DS(r1.G, [r1.getUindexofG(), 9])
+# print(ds.IN)
+# print(ds.OUT)
 
-print(ds.query(4))
-print(ds.query(4))
+# print(ds.query(4))
+# print(ds.query(4))
 
-print(ds.IN)
-print(ds.OUT)
+# print(ds.IN)
+# print(ds.OUT)
 
-ds.remove(3)
+# ds.remove(3)
 
-print(ds.IN)
-print(ds.OUT)
+# print(ds.IN)
+# print(ds.OUT)
 
-print(ds.query(1))
+# print(ds.query(1))
 
-print(r1.disjoint_paths)
-p, v = r1.preprocessEdgedisjointpaths(r1.disjoint_paths, r1.getUindex(), r1.getVindex())
-print(p)
-print(v)
+# print(r1.disjoint_paths)
+# p, v = r1.preprocessEdgedisjointpaths(r1.disjoint_paths, r1.getUindex(), r1.getVindex())
+# print(p)
+# print(v)
--- visualize.py
+++ visualize.py
@@ -19,8 +19,24 @@ class visualize:
         ax.margins(0.5, 0.5)
         plt.show()
 
-    def visualizeGraph(self, residualGraph):
+    def visualizeGraph(self, residualGraph, vertex_disjoint_paths, start, end):
         A = to_agraph(residualGraph)
+        A.node_attr['style']='filled'
+
+        # iterate over vertex disjoint paths
+        for path in vertex_disjoint_paths:
+                i = 0
+                while i < len(path) - 1:
+                    n = A.get_node(path[i])
+                    n.attr['fillcolor']="#6445d6"
+                    n = A.get_edge(path[i], path[i + 1])
+                    n.attr['fillcolor']="#6445d6"
+                    i = i + 1
+                # fill starting and ending node
+        n = A.get_node(start)
+        n.attr['fillcolor']="#d64945"
+        n = A.get_node(end)
+        n.attr['fillcolor']="#d64945"
         A.layout('dot')
         A.draw('graph.png')
         img = mpimg.imread('graph.png')
