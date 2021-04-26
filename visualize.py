ced15c8d6d43bb7c9330eee054eadf592c2b1879
Adiesha <adiesha@gmail.com>
2021-04-26 15:11:08 -0600

    added capability to show the graph on screen

--- visualize.py
+++ visualize.py
@@ -1,27 +1,28 @@
+import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-import networkx as nx
+from networkx.drawing.nx_agraph import to_agraph
 
-# import networkx as nx
-# import pylab as plt
-from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
-import pygraphviz as pgv
+
 class visualize:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def visualizeCircles(self, circles, belts, unitDistance):
-		fig, ax = plt.subplots()
-		# draw circles
-		for circle in circles:
-			print(circle)
-			ax.add_patch(plt.Circle((circle[0], circle[1]), unitDistance, color='b', fill=False))
-		# draw belts
-		ax.add_patch(plt.Rectangle(belts[0][0], belts[0][1], belts[0][2], color='r'))
-		ax.add_patch(plt.Rectangle(belts[1][0], belts[1][1], belts[1][2], color='r'))
-		ax.margins(0.5, 0.5) 
-		plt.show()
+    def visualizeCircles(self, circles, belts, unitDistance):
+        fig, ax = plt.subplots()
+        # draw circles
+        for circle in circles:
+            print(circle)
+            ax.add_patch(plt.Circle((circle[0], circle[1]), unitDistance, color='b', fill=False))
+        # draw belts
+        ax.add_patch(plt.Rectangle(belts[0][0], belts[0][1], belts[0][2], color='r'))
+        ax.add_patch(plt.Rectangle(belts[1][0], belts[1][1], belts[1][2], color='r'))
+        ax.margins(0.5, 0.5)
+        plt.show()
 
-	def visualizeGraph(self, residualGraph):
-		A = to_agraph(residualGraph)
-		A.layout('dot')
-		A.draw('graph.png')
+    def visualizeGraph(self, residualGraph):
+        A = to_agraph(residualGraph)
+        A.layout('dot')
+        A.draw('graph.png')
+        img = mpimg.imread('graph.png')
+        imgplot = plt.imshow(img)
+        plt.show()
